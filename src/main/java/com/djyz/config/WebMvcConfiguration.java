4f9bc7ec9b68e5330f6408219d96777b69742d2f
zouhang <zouhang@xiaomi.com>
2019-06-12 17:36:32 +0800

    还原数据库密码

--- src/main/java/com/djyz/config/WebMvcConfiguration.java
+++ src/main/java/com/djyz/config/WebMvcConfiguration.java
@@ -1,7 +1,6 @@
 package com.djyz.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -29,7 +28,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableWebMvc
 @EnableSwagger2
-@ComponentScan(basePackages = "com.djyz.web")
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Override
